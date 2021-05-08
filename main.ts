e49565c2d46fc0e9b506a05fa7de1995ad8cf374
ShawnReardon <shawnreardon1@gmail.com>
2021-05-08 14:52:24 -0400

    tailMechanics

--- main.ts
+++ main.ts
@@ -1,15 +1,62 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     transformSprites.rotateSprite(mySprite, 0)
+    newTail()
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     transformSprites.rotateSprite(mySprite, -90)
 })
+function initTail () {
+    tail = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    tail.follow(mySprite, 100)
+    lastTail = tail
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     transformSprites.rotateSprite(mySprite, 90)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     transformSprites.rotateSprite(mySprite, 180)
 })
+function newTail () {
+    tail = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    tail.follow(lastTail, 100)
+    lastTail = tail
+}
+let lastTail: Sprite = null
+let tail: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -30,3 +77,4 @@ mySprite = sprites.create(img`
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
+initTail()
