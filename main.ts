997fb49cb6ecd8d5d19e0373a3b4c3dd84c80219
ShawnReardon <shawnreardon1@gmail.com>
2021-05-08 17:11:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,22 +7,22 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function initTail () {
     tail = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+        . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, SpriteKind.Player)
     tail.follow(mySprite, 100)
     lastTail = tail
@@ -35,22 +35,22 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function newTail () {
     tail = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+        . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, SpriteKind.Player)
     tail.follow(lastTail, 100)
     lastTail = tail
@@ -59,22 +59,22 @@ let lastTail: Sprite = null
 let tail: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 6 6 6 2 2 6 6 6 . . . . 
+    . . . . 6 6 6 2 2 6 6 6 . . . . 
+    . . . 6 6 6 1 6 6 1 6 6 6 . . . 
+    . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+    . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+    . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+    . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+    . . . 6 6 6 6 6 6 6 6 6 6 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 initTail()
